4bc73673f7ee7c42882f2b250de5e0609a3b3286
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-03-23 23:38:06 +0530

    [BHUSHAN]Added:Taking Three Numbers and Performing Operation [A * B + C].

--- Arithmatic_Computation_And_Sorting.sh
+++ Arithmatic_Computation_And_Sorting.sh
@@ -5,5 +5,5 @@ echo "Enter THREE Number"
 read number1
 read number2
 read number3
-compute=$(($number1+$number2*$number3))
-echo "Compute:" $number1 "+" $number2 "*" $number3 "=" $compute
+compute=$(($number1*$number2+$number3))
+echo "Compute:" $number1 "*" $number2 "+" $number3 "=" $compute

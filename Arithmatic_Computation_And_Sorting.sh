67d1cfa84aa96974e2606d56dcf74d20e1e033d4
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-03-23 23:06:41 +0530

    [BHUSHAN]Added:Taking Three Numbers From User and Display them.

--- Arithmatic_Computation_And_Sorting.sh
+++ Arithmatic_Computation_And_Sorting.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 echo "Welecome to Arithmatic Computation and Sorting Programming"
+
+echo "Enter THREE Number"
+read number1
+read number2
+read number3
+echo "THREE Numbers are:" $number1 $number2 $number3

326d16c746e22edeb12e60031ea3f3e2857c0578
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-03-24 00:04:06 +0530

    [BHUSHAN]Added:Taking Three Inputs as Numbers and Store Result In Dictionary for every Computation.

--- Arithmatic_Computation_And_Sorting.sh
+++ Arithmatic_Computation_And_Sorting.sh
@@ -5,5 +5,24 @@ echo "Enter THREE Number"
 read number1
 read number2
 read number3
-compute=$(($number1%$number2+$number3))
-echo "Compute:" $number1 "%" $number2 "+" $number3 "=" $compute
+compute1=$(($number1+$number2*$number3))
+echo "Compute:" $number1 "+" $number2 "*" $number3 "=" $compute1
+
+compute2=$(($number1*$number2+$number3))
+echo "Compute:" $number1 "+" $number2 "*" $number3 "=" $compute2
+
+compute3=$(($number3+$number1/$number2))
+echo "Compute:" $number3 "+" $number1 "/" $number2 "=" $compute3
+
+compute4=$(($number1%$number2+$number3))
+echo "Compute:" $number1 "%" $number2 "+" $number3 "=" $compute4
+declare -A Result 
+Result[1]=$compute1
+Result[2]=$compute2
+Result[3]=$compute3
+Result[4]=$compute4
+
+echo "Result 1=" ${Result[1]}
+echo "Result 2=" ${Result[2]}
+echo "Result 3=" ${Result[3]}
+echo "Result 4=" ${Result[4]}
